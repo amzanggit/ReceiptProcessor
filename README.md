37ffd41cdb03e36c397d60ef9bfb5e3a30415966
Amanda Zhang <102166965+JSP22SCM04Z@users.noreply.github.com>
2023-09-25 23:19:20 -0500

    readme update

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# ReceiptProcessor
- Build a webservice that fulfils the documented API.
+# Receipt Processor
+
+ Build a webservice that fulfils the documented API. 
+
+## Environment Setup and Application Run
+This project is written in Go. To run this project, you need to have Go installed and set up on your local machine or IDE (I used IntelliJ).
+
+In your local IDE, search for the "Go" plugin and install it.
+ 
+Once the Go is installed in the IDE, follow these steps to run the application:
+
+1. **Clone Repository**: Download this repository to your local machine using
+   ```
+   git clone [repository_link_here]
+   ```
+   
+2. **Navigate to Repository**: Open your terminal and navigate to the repository folder.
+
+
+3. **Initialize Go Modules (if needed):** If you're using Go modules, ensure that you've initialized your project by running the following command:
+   ```
+   go mod init your_project_name
+   ```
+      This command initializes a go.mod file to manage your project's dependencies.
+
+
+4. **Install Dependencies**: Use the following command to download and install the project's dependencies:
+   ```
+   go mod tidy
+   ```
+
+5. **Run Application**: Execute the following command to run the application:
+   ```
+   go run main.go
+   ```
+
+## Application Test
--- main.go
+++ main.go
@@ -83,10 +83,10 @@ func CalculatePoints(receipt *Receipt) int {
 		points += 25
 	}
 
-	// Points based on trimmed item description length
+	// 5 points for every two items on the receipt
 	points += 5 * (len(receipt.Items) / 2)
 
-	// points based on trimmed item description length
+	//
 	for _, item := range receipt.Items {
 		if len(strings.TrimSpace(item.ShortDescription))%3 == 0 {
 			priceFloat, _ := strconv.ParseFloat(item.Price, 64)
@@ -94,37 +94,47 @@ func CalculatePoints(receipt *Receipt) int {
 		}
 	}
 
-	// 6 points if the day in purchase date is odd
-	if isPurchaseDateOdd(receipt.PurchaseDate) {
+	purchaseDate, err := time.Parse("2006-01-02", receipt.PurchaseDate)
+	if err == nil && purchaseDate.Day()%2 == 1 {
 		points += 6
 	}
 
-	// 10 points if the time of purchase is between 2pm and 4pm
-	if isPurchaseTimeBetweenTwoAndFour(receipt.PurchaseTime) {
+	purchaseTime, err = time.Parse("15:04", receipt.PurchaseTime)
+	if err == nil && purchaseTime.Hour() >= 14 && purchaseTime.Hour() < 16 {
 		points += 10
 	}
 
 	return points
 }
 
-func isPurchaseDateOdd(dateStr string) (bool, error) {
-	purchaseDate, err := time.Parse("2006-01-02", dateStr)
-	if err != nil {
-		// Return an error indicating there was an issue with parsing the date
-		return false, fmt.Errorf("failed to parse the date: %w", err)
-	}
-	return purchaseDate.Day()%2 == 1, nil
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/receipts/process", ProcessReceipt).Methods("POST")
+	r.HandleFunc("/receipts/{id}/points", GetPoints).Methods("GET")
+
+	http.Handle("/", r)
+	fmt.Println("Server started at :8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
 
-func isPurchaseTimeBetweenTwoAndFour(timeStr string) (bool, error) {
-	purchaseTime, err := time.Parse("15:04", timeStr)
-	if err != nil {
-		// Return an error indicating there was an issue with parsing the time
-		return false, fmt.Errorf("failed to parse the time: %w", err)
-	}
-
-	start, _ := time.Parse("15:04", "13:59")
-	end, _ := time.Parse("15:04", "16:01")
-
-	return purchaseTime.After(start) && purchaseTime.Before(end), nil
-}
+//func isPurchaseDateOdd(dateStr string) (bool, error) {
+//	purchaseDate, err := time.Parse("2006-01-02", dateStr)
+//	if err != nil {
+//		// Return an error indicating there was an issue with parsing the date
+//		return false, fmt.Errorf("failed to parse the date: %w", err)
+//	}
+//	return purchaseDate.Day()%2 == 1, nil
+//}
+//
+//func isPurchaseTimeBetweenTwoAndFour(timeStr string) (bool, error) {
+//	purchaseTime, err := time.Parse("15:04", timeStr)
+//	if err != nil {
+//		// Return an error indicating there was an issue with parsing the time
+//		return false, fmt.Errorf("failed to parse the time: %w", err)
+//	}
+//
+//	start, _ := time.Parse("15:04", "13:59")
+//	end, _ := time.Parse("15:04", "16:01")
+//
+//	return purchaseTime.After(start) && purchaseTime.Before(end), nil
+//}
